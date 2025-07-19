15fc800b32c32982fe1b6abb1df6c45759acb241
vladimir-semyonov <145898013+vladimir-semyonov@users.noreply.github.com>
2025-07-19 13:49:03 +0300

    0.0.2

--- calc.py
+++ calc.py
@@ -51,21 +51,37 @@ while v == True:
                 n2 = int(input("Введите второе число: "))
                 print(n1//n2, "", n1%n2)
         elif v2 == 5:
-            n = int(input())
+            n = int(input("Введите число: "))
             print(n**2)
         elif v2 == 6:
-            n = int(input())
-            s = int(input())
+            n = int(input("Введите число: "))
+            s = int(input("Введите степень: "))
             print(n**s)
         elif v2 == 7:
-            print()
+            a = int(input("a:"))
+            b = int(input("b:"))
+            c = int(input("c:"))
+            D = (b**2) - (4*a*c)
+            if D > 0:
+                x1 = (-b + math.sqrt(D))/(2*a)
+                x2 = (-b - math.sqrt(D))/(2*a)
+                if x1 == x2:
+                    print(x1)
+                else: print(x1,x2)
+            elif D == 0:
+                x1 = -b/(2*a)
+                print(x1)
+            else: print("error")
         elif v2 == 8:
-            n = int(input())
-            z = int(input())
+            n = int(input("Введите число: "))
+            z = int(input("Введите кол-во знаков после запятой: "))
             print(round(n, z))
         elif v2 == 9:
-            n = int(input())
+            n = int(input("Введите число: "))
             print(math.factorial(n))
+    elif v1 == 3:
+        v = False
+        print("goodbay")
 
 
     elif v1 == 3:
